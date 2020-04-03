64d267ce24d1c40096d721707bc101163da0a092
mpottebaum <mpottebaum@gmail.com>
2020-04-03 18:14:44 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+  let gameWin = record.find(game => game.result === "W")
+  if (gameWin) {
+    return gameWin.year
+  } else {
+    return gameWin
+  }
+}
