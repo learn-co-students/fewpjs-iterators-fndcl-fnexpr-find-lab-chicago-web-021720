f44ab86c5210a5771c1be1f30120ee52638a7ca8
deber116 <deber116@gmail.com>
2020-04-06 00:27:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(record){
+  let result = record.find(function(s) {return (s["result"] === "W")})
+  if (result) {
+    result = result["year"]
+  }
+  return result
+  
+  
 }
