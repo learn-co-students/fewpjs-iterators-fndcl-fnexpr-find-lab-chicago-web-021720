b89f89c9b0d4e1493eb73d2bea5288235cec4275
Ethan Davidson <theejdavidson@gmail.com>
2020-04-07 21:40:10 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const winObj = record.find( function(r) {
+    return r['result'] === "W"
+  });
+  if(winObj) {
+    return winObj['year'];
+  }
+}
